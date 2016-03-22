18e5d968ee598e0a6543835c36b668b6dd638d95
Ricardo Melo <ricardo.d.melo@hotmail.com>
2016-03-22 01:54:17 -0300

    Changes on Description Filter.

--- common/filters/filter-description.js
+++ common/filters/filter-description.js
@@ -4,13 +4,19 @@ var log = require('winston');
 module.exports = function (obj, $, options) {
     if (!obj.description) {
         obj.description = '';
+        log.info('Getting Description: ' + obj.description);
 
         if ($("meta").is("[property='og:description']")) {
             obj.description = $("meta[property='og:description']").attr('content');
-        } else if ($("meta").is("[property='Description']")) {
+            log.info('Getting Description1: ' + obj.description);
+        } else if ($("meta").is("[name='Description']")) {
             obj.description = $("meta[name='Description']").attr('content');
-        } else if ($("meta").is("[property='description']")) {
+        } else if ($("meta").is("[name='description']")) {
             obj.description = $("meta[name='description']").attr('content');
+        } else if ($("meta").is("[property='Description']")) {
+            obj.description = $("meta[property='Description']").attr('content');
+        } else if ($("meta").is("[property='description']")) {
+            obj.description = $("meta[property='description']").attr('content');
         }
     }
 
