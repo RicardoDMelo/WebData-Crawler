4f0e734f6e484f0b533b69e6892e78d09d7744c0
Ricardo Melo <ricardo.d.melo@hotmail.com>
2016-03-22 02:01:38 -0300

    Bugfix on description filter

--- common/filters/filter-description.js
+++ common/filters/filter-description.js
@@ -4,11 +4,9 @@ var log = require('winston');
 module.exports = function (obj, $, options) {
     if (!obj.description) {
         obj.description = '';
-        log.info('Getting Description: ' + obj.description);
 
         if ($("meta").is("[property='og:description']")) {
             obj.description = $("meta[property='og:description']").attr('content');
-            log.info('Getting Description1: ' + obj.description);
         } else if ($("meta").is("[name='Description']")) {
             obj.description = $("meta[name='Description']").attr('content');
         } else if ($("meta").is("[name='description']")) {
